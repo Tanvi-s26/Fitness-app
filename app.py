f0f6a9339ee9483707c3b8616343e5976c31a0bd
Tanvi <tanviijanday@gmail.com>
2026-05-15 14:17:39 +0530

    Day 1 : initial Flask setup

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to FitTrack!"
+
+@app.route("/health")
+def health():
+    return "Server is running!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
