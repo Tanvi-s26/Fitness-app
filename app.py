14f4bb104df8ea6f7d5cc34246a12243acefa316
Tanvi <tanviijanday@gmail.com>
2026-05-16 12:44:08 +0530

    Day 2

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to FitTrack!"
+    return render_template("index.html")
 
-@app.route("/health")
-def health():
-    return "Server is running!"
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+        # We'll save to database on Day 3
+        print(f"New user: {username}, {email}")
+        return f"Welcome {username}! Database coming Day 3 😄"
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
