d47622f86bef9480e66f21c956ccb79c78ada390
Tanvi <tanviijanday@gmail.com>
2026-05-18 22:23:10 +0530

    Database

---
+++ models/workout.py
@@ -0,0 +1,26 @@
+from models.user import db
+from datetime import datetime
+
+class Workout(db.Model):
+    __tablename__ = "workouts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    workout_type = db.Column(db.String(100), nullable=False)
+    duration = db.Column(db.Integer, nullable=False)  # in minutes
+    calories = db.Column(db.Integer, nullable=False)
+    notes = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship("User", backref="workouts")
+
+class Follow(db.Model):
+    __tablename__ = "follows"
+
+    id = db.Column(db.Integer, primary_key=True)
+    follower_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    following_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Workout {self.workout_type} by user {self.user_id}>"
